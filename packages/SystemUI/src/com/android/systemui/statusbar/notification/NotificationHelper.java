22b2d6547390b216fcf3f4337ed830aabeaeaeed
BigBrother1984 <stevewatersy@gmail.com>
2014-06-06 20:24:41 +0200

    NotificationHelper: We shouldn't use logical here, one case is enough no matter the order
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/NotificationHelper.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/NotificationHelper.java
@@ -195,7 +195,6 @@ public class NotificationHelper {
         }
     }
 
-
     public boolean isRingingOrConnected() {
         return mRingingOrConnected;
     }
@@ -203,7 +202,7 @@ public class NotificationHelper {
     public boolean isSimPanelShowing() {
         int state = mTelephonyManager.getSimState();
         return state == TelephonyManager.SIM_STATE_PIN_REQUIRED
-                 || state == TelephonyManager.SIM_STATE_PUK_REQUIRED
-                 || state == TelephonyManager.SIM_STATE_NETWORK_LOCKED;
+                 | state == TelephonyManager.SIM_STATE_PUK_REQUIRED
+                 | state == TelephonyManager.SIM_STATE_NETWORK_LOCKED;
     }
 }
